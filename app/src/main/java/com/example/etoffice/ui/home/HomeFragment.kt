d06e40b858801bc3f4a627965bf6c30e7aeb549a
chenzhizs <chenzhi@qq.com>
2021-06-29 12:27:06 +0900

    主画面，前端设计完成30%

--- app/src/main/java/com/example/etoffice/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/etoffice/ui/home/HomeFragment.kt
@@ -21,10 +21,32 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
+        })
+
+        val textTitle1: TextView = root.findViewById(R.id.text_title1)
+        homeViewModel.title1.observe(viewLifecycleOwner, Observer {
+            textTitle1.text = it
+        })
+
+        val textTitle2: TextView = root.findViewById(R.id.text_title2)
+        homeViewModel.title2.observe(viewLifecycleOwner, Observer {
+            textTitle2.text = it
+        })
+
+        val textTitle3: TextView = root.findViewById(R.id.text_title3)
+        homeViewModel.title3.observe(viewLifecycleOwner, Observer {
+            textTitle3.text = it
+        })
+
+        val textTitle4: TextView = root.findViewById(R.id.text_title4)
+        homeViewModel.title4.observe(viewLifecycleOwner, Observer {
+            textTitle4.text = it
         })
         return root
     }
--- app/src/main/java/com/example/etoffice/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/etoffice/ui/home/HomeViewModel.kt
@@ -7,7 +7,27 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "ホーム"
     }
     val text: LiveData<String> = _text
+
+    private val _title1 = MutableLiveData<String>().apply {
+        value = "_title1"
+    }
+    val title1: LiveData<String> = _title1
+
+    private val _title2 = MutableLiveData<String>().apply {
+        value = "_title2"
+    }
+    val title2: LiveData<String> = _title2
+
+    private val _title3 = MutableLiveData<String>().apply {
+        value = "_title3"
+    }
+    val title3: LiveData<String> = _title3
+
+    private val _title4 = MutableLiveData<String>().apply {
+        value = "_title4"
+    }
+    val title4: LiveData<String> = _title4
 }
