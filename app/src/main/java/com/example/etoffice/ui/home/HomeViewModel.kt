a841d14328247730592d54750713cf4734003fbf
chenzhizs <chenzhi@qq.com>
2021-06-29 15:58:15 +0900

    主画面，前端设计完成80%

--- app/src/main/java/com/example/etoffice/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/etoffice/ui/home/HomeViewModel.kt
@@ -12,22 +12,22 @@ class HomeViewModel : ViewModel() {
     val text: LiveData<String> = _text
 
     private val _title1 = MutableLiveData<String>().apply {
-        value = "_title1"
+        value = "株式会社"
     }
     val title1: LiveData<String> = _title1
 
     private val _title2 = MutableLiveData<String>().apply {
-        value = "_title2"
+        value = "共通機能"
     }
     val title2: LiveData<String> = _title2
 
     private val _title3 = MutableLiveData<String>().apply {
-        value = "_title3"
+        value = "出勤記録"
     }
     val title3: LiveData<String> = _title3
 
     private val _title4 = MutableLiveData<String>().apply {
-        value = "_title4"
+        value = "Message"
     }
     val title4: LiveData<String> = _title4
 }
