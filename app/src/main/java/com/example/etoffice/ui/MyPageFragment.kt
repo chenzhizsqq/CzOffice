5426ad9e5cbd41f8a38d83d39dcb66539e44ada7
chenzhizs <chenzhi@qq.com>
2021-06-30 17:53:13 +0900

    主画面，第5个次画面 -》作业场所管理 补充「ログアウト」功能

--- app/src/main/java/com/example/etoffice/ui/MyPageFragment.kt
+++ app/src/main/java/com/example/etoffice/ui/MyPageFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.etoffice.LoginActivity
 import com.example.etoffice.R
 
 
@@ -22,20 +24,36 @@ class MyPageFragment : Fragment() {
         var testTV:TextView= root.findViewById(R.id.user_name)
         testTV.text = "my page ok"
 
+        //Place　Setting
         val pImageView: ImageView = root.findViewById(R.id.imageView_MyPage_Logo) as ImageView
         pImageView.setOnClickListener(View.OnClickListener {
             val intent = Intent(activity, MyPagePlaceSettingActivity::class.java)
             startActivity(intent)
         })
 
+        //change　company
         val pTableRow: TableRow = root.findViewById(R.id.change_company) as TableRow
         pTableRow.setOnClickListener(View.OnClickListener {
             val intent = Intent(activity, MyPageChangeCompanyActivity::class.java)
             startActivity(intent)
         })
+
+
+        //ログアウト
+        val pTableLayout: TableLayout = root.findViewById(R.id.SYSTEM_info) as TableLayout
+        pTableLayout.setOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        })
+
+
         return root
     }
 
-
+    companion object {
+        fun newInstance(): MyPageFragment {
+            return MyPageFragment()
+        }
+    }
 
 }
