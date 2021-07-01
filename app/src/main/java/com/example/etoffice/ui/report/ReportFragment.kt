60dc9dd6eee768dd99e4d6fdda5316fd629d67fe
chenzhizs <chenzhi@qq.com>
2021-07-01 12:09:52 +0900

    主画面，第四个次画面 -》 日报详细页面 基本设计80%

--- app/src/main/java/com/example/etoffice/ui/report/ReportFragment.kt
+++ app/src/main/java/com/example/etoffice/ui/report/ReportFragment.kt
@@ -1,11 +1,14 @@
 package com.example.etoffice.ui.report
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import androidx.fragment.app.Fragment
 import com.example.etoffice.R
+import com.example.etoffice.ui.MyPage.MyPagePlaceSettingActivity
 
 class ReportFragment : Fragment() {
 
@@ -14,7 +17,17 @@ class ReportFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_scrolling_report, container, false)
+        val root = inflater.inflate(R.layout.fragment_scrolling_report, container, false)
+
+
+        //report_info_title
+        val pTableRowInfoTitle: TableRow = root.findViewById(R.id.report_info_title_1) as TableRow
+        pTableRowInfoTitle.setOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, ReportDetail::class.java)
+            startActivity(intent)
+        })
+
+        return root
     }
 
     companion object {
