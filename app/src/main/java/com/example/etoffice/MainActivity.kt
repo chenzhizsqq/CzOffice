137ed30f84454fb72cc1433d1ccfe471de4213f5
chenzhizs <chenzhi@qq.com>
2021-07-02 11:48:50 +0900

    主画面，第3个次画面：メンバー　 完成20%

--- app/src/main/java/com/example/etoffice/MainActivity.kt
+++ app/src/main/java/com/example/etoffice/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home
             , R.id.navigation_dashboard
-            , R.id.navigation_notifications
+            , R.id.member_fragment
             ,R.id.ScrollingFragmentReport
             ,R.id.MyPageFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
