841d837be82438af70e9071c51690c52c94d8841
chenzhizs <chenzhi@qq.com>
2021-07-02 15:25:19 +0900

    主画面，第3个次画面：メンバー　 電話しる機能を付けます

--- app/src/main/java/com/example/etoffice/ui/member/MemberFragment.kt
+++ app/src/main/java/com/example/etoffice/ui/member/MemberFragment.kt
@@ -1,20 +1,36 @@
 package com.example.etoffice.ui.member
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.etoffice.R
 
 class MemberFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_member, container, false)
+        val root = inflater.inflate(R.layout.fragment_member, container, false)
+
+
+        //call_telephone 電話します
+        val pTableRowInfoTitle: TextView = root.findViewById(R.id.call_telephone) as TextView
+        pTableRowInfoTitle.setOnClickListener(View.OnClickListener {
+            var textTitle:CharSequence = pTableRowInfoTitle.text;
+            val uri: Uri = Uri.parse("tel:"+textTitle)
+            val intent = Intent(Intent.ACTION_CALL, uri)
+            startActivity(intent)
+        })
+
+        return root
+
     }
 }
