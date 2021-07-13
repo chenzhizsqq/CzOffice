bd46b9d2b45bf1bcdfa7bc851712728256d25987
chenzhizs <chenzhi@qq.com>
2021-07-13 12:23:38 +0900

    创建自创工具栏 工具栏加入：时间处理平台

---
+++ app/src/main/java/com/xieyi/etoffice/Tools.kt
@@ -0,0 +1,41 @@
+package com.xieyi.etoffice
+
+object Tools {
+    private const val TAG = "Tools"
+
+    fun dataChange(_data: String?, _insert: String?): String {
+        var rStr = _data
+        val sb = StringBuilder(rStr)
+        sb.insert(6, _insert)
+        sb.insert(4, _insert)
+        rStr = sb.toString()
+        return rStr
+    }
+
+    fun srcContent(src: String, maxLength: Int): String {
+        var rString = src
+        if (rString.length > maxLength) {
+            rString = src.substring(0, maxLength)
+            rString += "..."
+        }
+        return rString
+    }
+
+    fun dataGetYear(_data: String): Int {
+        if (_data.isEmpty()) return 0
+        val str = _data.substring(0, 4)
+        return str.toInt()
+    }
+
+    fun dataGetMonth(_data: String): Int {
+        if (_data.isEmpty()) return 0
+        val str = _data.substring(4, 6)
+        return str.toInt()
+    }
+
+    fun dataGetDay(_data: String): Int {
+        if (_data.isEmpty()) return 0
+        val str = _data.substring(6, 8)
+        return str.toInt()
+    }
+}
