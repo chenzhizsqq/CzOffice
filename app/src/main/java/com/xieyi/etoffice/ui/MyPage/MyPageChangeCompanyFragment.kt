f77e4172877d5b642e8fd83fae180548beb7579b
chenzhizs <chenzhi@qq.com>
2021-07-23 10:24:05 +0900

    针对 Kotlin 代码，使用 Kotlin coroutines 。"11 切替" :MyPageChangeCompanyFragment 在 activity_my_page_change_company.xml 中。对应数据 "EtOfficeGetTenant"  完成90%

--- app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyFragment.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyFragment.kt
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.xieyi.etoffice.R
 import com.xieyi.etoffice.jsonData.JC
+import kotlinx.coroutines.*
 
 
 class MyPageChangeCompanyFragment : Fragment() {
@@ -29,128 +30,139 @@ class MyPageChangeCompanyFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.e(TAG, "onCreate: begin", )
-        Thread {
-            try {
-
-                val r = JC.pEtOfficeGetTenant.post()                                    //Json 送信
-                Log.e(TAG, "pEtOfficeGetTenant.post() :$r")
-
-
-            }catch (e:Exception){
-                Log.e(TAG, "pEtOfficeGetTenant.post() :$e")
-
-            }
-        }.start()
     }
 
+    private lateinit var mainView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.activity_my_page_change_company, container, false)
+        mainView = inflater.inflate(R.layout.activity_my_page_change_company, container, false)
 
-        val recordLinearLayout = view.findViewById<LinearLayout>(R.id.record_linearLayout)
-
-
-        //Log.e(TAG, "JC.pEtOfficeGetTenant:"+JC.pEtOfficeGetTenant.lastJson )
-
-        val size= JC.pEtOfficeGetTenant.infoJson().result.tenantlist.size
-
-        for (i in 0 .. size-1){
-
-            val mLinearLayout= LinearLayout(activity)
-
-            mLinearLayout.setOrientation(LinearLayout.VERTICAL)
-
-            mLinearLayout.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-
-
-            //mLinearLayout tag
-            mLinearLayout.tag = tagName+"_"+i
-
-
-            //up
-            val textView = TextView(activity)
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
-            textView.setTextColor(Color.parseColor("#000000"))
-            textView.text = JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantname
-            mLinearLayout.addView(textView)
-
-
-            //down
-            val textViewRight = TextView(activity)
-            textViewRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
-            textViewRight.setTextColor(Color.parseColor("#000000"))
-            textViewRight.text = JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].posturl
-            mLinearLayout.addView(textViewRight)
-
-
-            mLinearLayout.setBackgroundColor(Color.WHITE)
-            mLinearLayout.setPadding(30)
-
-            //check tenantid
-            if(JC.pEtOfficeGetTenant.infoJson().result.tenantlist[0].tenantid == JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantid){
-                mLinearLayout.setBackgroundColor(Color.GREEN)
-            }
-
-
-
-            //mLinearLayout touch   begin
-            mLinearLayout.setOnClickListener {
+        GlobalScope.launch(errorHandler) {
+            withContext(Dispatchers.IO) {
+                //データ更新
                 try {
-                    //
-                    for (j in 0 .. size-1){
-                        val ll=recordLinearLayout.findViewWithTag<LinearLayout>(tagName+"_"+j)
-                        ll.setBackgroundColor(Color.WHITE)
-                    }
-
-                    val r:String = JC.pEtOfficeSetTenant.post(JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantid)
-                    Log.e(TAG, "tenantid post r="+r )
-
-
-                    //check tenantid
-                    if(r=="0"){
-                        mLinearLayout.setBackgroundColor(Color.GREEN)
-                    }
+                    val r = JC.pEtOfficeGetTenant.post()                                    //Json 送信
+                    Log.e(TAG, "pEtOfficeGetTenant.post() :$r")
 
                 }catch (e:Exception){
-                    Log.e(TAG, "mLinearLayout.setOnClickListener: ",e )
+                    Log.e(TAG, "pEtOfficeGetTenant.post() :$e")
                 }
+
+                doOnUiCode()
             }
-            //mLinearLayout touch end
-
-
-
-            recordLinearLayout.addView(mLinearLayout)
-
-
-
-            //線
-
-            val mLinearLayout2= LinearLayout(activity)
-            val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
-            mLinearLayout2.layoutParams = lp2
-            mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
-            recordLinearLayout.addView(mLinearLayout2)
-
         }
 
 
-        //returnHome
-        val returnHome = view.findViewById<ImageView>(R.id.returnHome)
-        returnHome.setOnClickListener {
-
-            Navigation.findNavController(view)
-                .navigate(R.id.MyPageFragment);
-
-        }
-
-
-        return view
+        return mainView
     }
 
-    fun check(){
+    private val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        // 发生异常时的捕获
+    }
 
+    private suspend fun doOnUiCode() {
+        withContext(Dispatchers.Main) {
+            val recordLinearLayout = mainView.findViewById<LinearLayout>(R.id.record_linearLayout)
+
+
+            //Log.e(TAG, "JC.pEtOfficeGetTenant:"+JC.pEtOfficeGetTenant.lastJson )
+
+            val size = JC.pEtOfficeGetTenant.infoJson().result.tenantlist.size
+
+            for (i in 0..size - 1) {
+
+                val mLinearLayout = LinearLayout(activity)
+
+                mLinearLayout.setOrientation(LinearLayout.VERTICAL)
+
+                mLinearLayout.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+
+
+                //mLinearLayout tag
+                mLinearLayout.tag = tagName + "_" + i
+
+
+                //up
+                val textView = TextView(activity)
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
+                textView.setTextColor(Color.parseColor("#000000"))
+                textView.text = JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantname
+                mLinearLayout.addView(textView)
+
+
+                //down
+                val textViewRight = TextView(activity)
+                textViewRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
+                textViewRight.setTextColor(Color.parseColor("#000000"))
+                textViewRight.text = JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].posturl
+                mLinearLayout.addView(textViewRight)
+
+
+                mLinearLayout.setBackgroundColor(Color.WHITE)
+                mLinearLayout.setPadding(30)
+
+                //check tenantid
+                if (JC.pEtOfficeGetTenant.infoJson().result.tenantlist[0].tenantid == JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantid) {
+                    mLinearLayout.setBackgroundColor(Color.GREEN)
+                }
+
+
+                //mLinearLayout touch   begin
+                mLinearLayout.setOnClickListener {
+                    GlobalScope.launch(errorHandler) {
+                        withContext(Dispatchers.IO) {
+                            try {
+                                //
+                                for (j in 0..size - 1) {
+                                    val ll =
+                                        recordLinearLayout.findViewWithTag<LinearLayout>(tagName + "_" + j)
+                                    ll.setBackgroundColor(Color.WHITE)
+                                }
+
+                                val r: String =
+                                    JC.pEtOfficeSetTenant.post(JC.pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantid)
+                                Log.e(TAG, "tenantid post r=" + r)
+
+
+                                //check tenantid
+                                if (r == "0") {
+                                    mLinearLayout.setBackgroundColor(Color.GREEN)
+                                }
+
+                            } catch (e: Exception) {
+                                Log.e(TAG, "mLinearLayout.setOnClickListener: ", e)
+                            }
+                        }
+                    }
+                }
+                //mLinearLayout touch end
+
+
+                recordLinearLayout.addView(mLinearLayout)
+
+
+                //線
+
+                val mLinearLayout2 = LinearLayout(activity)
+                val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
+                mLinearLayout2.layoutParams = lp2
+                mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
+                recordLinearLayout.addView(mLinearLayout2)
+
+            }
+
+
+            //returnHome
+            val returnHome = mainView.findViewById<ImageView>(R.id.returnHome)
+            returnHome.setOnClickListener {
+
+                Navigation.findNavController(mainView)
+                    .navigate(R.id.MyPageFragment);
+
+            }
+        }
     }
 }
