905df6878c3805f31391b8125ddf7ab0488cdd1a
chenzhizs <chenzhi@qq.com>
2021-08-16 16:12:48 +0900

    "15	切り替え MyPageChangeCompanyActivity.kt" 70% 差不多完成了，只剩下前端的显示功能

--- app/src/main/java/com/xieyi/etoffice/ui/MyPage/GetTenantAdapter.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/MyPage/GetTenantAdapter.kt
@@ -8,13 +8,27 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.xieyi.etoffice.R
 import com.xieyi.etoffice.jsonData.EtOfficeGetTenant
+import com.xieyi.etoffice.jsonData.EtOfficeSetTenant
+import kotlinx.coroutines.*
 
 
-class GetTenantAdapter(val getGetTenant: List<EtOfficeGetTenant.Tenantlist>, val context: Context
-) :
-    RecyclerView.Adapter<GetTenantAdapter.sectionListViewHolder>() {
+class GetTenantAdapter(
+    val getGetTenant: List<EtOfficeGetTenant.Tenantlist>,
+    val context: Context,
+) : RecyclerView.Adapter<GetTenantAdapter.sectionListViewHolder>() {
     val TAG:String = javaClass.simpleName
 
+    private lateinit var pEtOfficeSetTenant : EtOfficeSetTenant
+
+    private lateinit var listener: OnAdapterListener
+
+    interface OnAdapterListener  {
+        fun onClick(tenantid:String)
+    }
+
+    fun setOnAdapterListener(adapterListener: OnAdapterListener) {
+        this.listener = adapterListener
+    }
 
     class sectionListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tv_userkana: TextView = itemView.findViewById(R.id.userkana)
@@ -23,22 +37,33 @@ class GetTenantAdapter(val getGetTenant: List<EtOfficeGetTenant.Tenantlist>, val
         private val tv_mail: TextView = itemView.findViewById(R.id.mail)
 
 
-        private val ll: LinearLayout = itemView.findViewById(R.id.ll)
+        val ll: LinearLayout = itemView.findViewById(R.id.ll)
+
 
         //telephone
-        fun bind(stufflist: EtOfficeGetTenant.Tenantlist,context: Context) {
+        fun bind(stufflist: EtOfficeGetTenant.Tenantlist,context: Context,listener:OnAdapterListener) {
             tv_userkana.text = stufflist.hpid
             tv_username.text = stufflist.posturl
             tv_phone.text = stufflist.startflg
             tv_mail.text = stufflist.tenantname
 
+            ll.setOnClickListener(View.OnClickListener {
+                listener.onClick(stufflist.tenantid)
+            })
+
         }
+    }
+
+    private val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        // 发生异常时的捕获
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): sectionListViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.get_tenant_list, parent, false)
 
+
+        pEtOfficeSetTenant = EtOfficeSetTenant()
         
         return sectionListViewHolder(view)
     }
@@ -48,6 +73,6 @@ class GetTenantAdapter(val getGetTenant: List<EtOfficeGetTenant.Tenantlist>, val
     }
 
     override fun onBindViewHolder(holder: sectionListViewHolder, position: Int) {
-        holder.bind(getGetTenant[position],context)
+        holder.bind(getGetTenant[position],context,listener)
     }
 }
--- app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyActivity.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyActivity.kt
@@ -21,7 +21,6 @@ import com.xieyi.etoffice.R
 import com.xieyi.etoffice.jsonData.EtOfficeGetTenant
 import com.xieyi.etoffice.jsonData.EtOfficeLogin
 import com.xieyi.etoffice.jsonData.EtOfficeSetTenant
-
 import kotlinx.coroutines.*
 
 
@@ -43,7 +42,7 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
 
     private lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
     private lateinit var mRecyclerView: RecyclerView
-    private lateinit var mAdapter: GetTenantAdapter
+    private lateinit var mAdapter:GetTenantAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,10 +53,9 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
         pEtOfficeSetTenant = EtOfficeSetTenant()
         pEtOfficeLogin = EtOfficeLogin()
 
-        mSwipeRefreshLayout= findViewById(R.id.swipeRefreshLayout)
+        refreshPage()
 
-        // Listenerをセット
-        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout= findViewById(R.id.swipeRefreshLayout)
 
         mRecyclerView = findViewById(R.id.recycler_view_get_tenant)
         mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
@@ -65,8 +63,9 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
                 super.onScrollStateChanged(recyclerView, newState)
                 if(newState == RecyclerView.SCROLL_STATE_IDLE){
                     if(!recyclerView.canScrollVertically(1)){
-                        Log.e(TAG, "onScrollStateChanged: more date", )
+                        //Log.e(TAG, "onScrollStateChanged: more date")
                         mSwipeRefreshLayout.isRefreshing = false
+                        refreshPage()
 
                     }
                 }
@@ -74,7 +73,8 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
             }
         })
 
-        refreshPage()
+        // Listenerをセット
+        mSwipeRefreshLayout.setOnRefreshListener(this);
     }
 
     private fun refreshPage() {
@@ -102,7 +102,7 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
     // UI更新
     private suspend fun doOnUiRefresh() {
         withContext(Dispatchers.Main) {
-            Log.e(TAG, "doOnUiRefresh: begin", )
+            //Log.e(TAG, "doOnUiRefresh: begin")
 
             //record_title
             val recordTitle = findViewById<TextView>(R.id.record_title)
@@ -122,213 +122,47 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
 
 
             val recyclerView: RecyclerView = findViewById(R.id.recycler_view_get_tenant)
-            recyclerView.adapter = GetTenantAdapter(pEtOfficeGetTenant.infoJson().result.tenantlist,this@MyPageChangeCompanyActivity)
 
-        }
-    }
-
-    private suspend fun doOnUiCodeOld() {
-        withContext(Dispatchers.Main) {
-            val recordLinearLayout = findViewById<LinearLayout>(R.id.record_linearLayout)
-            recordLinearLayout.removeAllViews()
-
-            //record_title
-            val recordTitle = findViewById<TextView>(R.id.record_title)
-            val tenantid = EtOfficeApp.TenantId
-            val hpid = EtOfficeApp.HpId
-            recordTitle.text = "TENANTID = $tenantid HPID = $hpid"
-
-            //Log.e(TAG, "pEtOfficeGetTenant:"+pEtOfficeGetTenant.lastJson )
-
-            val size = pEtOfficeGetTenant.infoJson().result.tenantlist.size
-
-            for (i in 0..size - 1) {
-
-                val mLinearLayout = LinearLayout(applicationContext)
-
-                mLinearLayout.setOrientation(LinearLayout.VERTICAL)
-
-                mLinearLayout.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+            val sortedList = pEtOfficeGetTenant.infoJson().result.tenantlist.sortedWith(compareBy(
+                { it.tenantid }
+                , { it.tenantname }
+                , { it.hpid }
+                , { it.posturl }
+            ))
+            mAdapter=GetTenantAdapter(sortedList,this@MyPageChangeCompanyActivity)
 
 
-                //mLinearLayout tag
-                mLinearLayout.tag = tagName + "_" + i
-
-                val ll_mm = LinearLayout(applicationContext)
-
-                ll_mm.setOrientation(LinearLayout.HORIZONTAL)
-
-                ll_mm.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                ll_mm.gravity = (Gravity.LEFT or Gravity.CENTER)
-
-                val ll_left = leftLL(i)
-
-
-                ll_mm.addView(ll_left)
-
-                val ll_right = rightLL()
-                ll_right.visibility = View.GONE
-                ll_right.tag = logoTag(i)
-                ll_mm.addView(ll_right)
-
-
-                mLinearLayout.addView(ll_mm)
-
-
-                mLinearLayout.setBackgroundColor(Color.WHITE)
-                mLinearLayout.setPadding(30)
-
-                //check tenantid
-                if(pEtOfficeGetTenant.infoJson().result.tenantlist[i].startflg=="1"){
-                    mLinearLayout.setBackgroundColor(Color.GREEN)
-
-                    ll_right.visibility = View.VISIBLE
-                }
-
-
-                //データ　発信
-                mLinearLayout.setOnClickListener {
-
-                    //
-                    for (j in 0..size - 1) {
-                        val ll =
-                            recordLinearLayout.findViewWithTag<LinearLayout>(tagName + "_" + j)
-                        ll.setBackgroundColor(Color.WHITE)
-
-                        val rightLL=
-                            recordLinearLayout.findViewWithTag<LinearLayout>(logoTag(j))
-                        rightLL.visibility = View.GONE
-                    }
-
-                    ll_right.visibility = View.VISIBLE
+            mAdapter.setOnAdapterListener(object : GetTenantAdapter.OnAdapterListener{
+                override fun onClick(tenantid: String) {
 
                     GlobalScope.launch(errorHandler) {
                         withContext(Dispatchers.IO) {
+                            //データ更新 SetTenant
                             try {
-
-                                val r: String =
-                                    pEtOfficeSetTenant.post(pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantid)
-                                Log.e(TAG, "tenantid post r=" + r)
-
-
-                                //check tenantid
-                                if (r == "0") {
-                                    for (k in  0 .. pEtOfficeSetTenant.infoJson().result.tenantlist.size -1 ){
-
-                                        if(pEtOfficeSetTenant.infoJson().result.tenantlist[k].startflg=="1"){
-                                            mLinearLayout.setBackgroundColor(Color.GREEN)
-
-
-
-                                            EtOfficeApp.TenantId =
-                                                pEtOfficeSetTenant.infoJson().result.tenantlist[k].tenantid
-                                            EtOfficeApp.HpId =
-                                                pEtOfficeSetTenant.infoJson().result.tenantlist[k].hpid
-
-                                            val r: String =
-                                                pEtOfficeLogin.post(EtOfficeApp.uid,EtOfficeApp.password)
-                                            Log.e(TAG, "pEtOfficeLogin.post r=" + r)
-
-
-                                            recordTitle.text = "TENANTID = "+EtOfficeApp.TenantId+" HPID = "+EtOfficeApp.HpId
-                                        }
-
+                                if(pEtOfficeSetTenant.post(tenantid)=="0"){
+                                    if(pEtOfficeLogin.post(EtOfficeApp.uid,EtOfficeApp.password) == "0"){
+                                        refreshPage()
                                     }
                                 }
-
                             } catch (e: Exception) {
-                                Log.e(TAG, "mLinearLayout.setOnClickListener: ", e)
+                                Log.e(TAG, "pEtOfficeSetTenant.post()",e)
                             }
+
                         }
                     }
+
                 }
-                //mLinearLayout touch end
+            })
 
+            recyclerView.adapter = mAdapter
 
-                recordLinearLayout.addView(mLinearLayout)
-
-
-                //線
-
-                val mLinearLayout2 = LinearLayout(applicationContext)
-                val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
-                mLinearLayout2.layoutParams = lp2
-                mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
-                recordLinearLayout.addView(mLinearLayout2)
-
-            }
-
-
-            //returnHome
-            val returnHome = findViewById<ImageView>(R.id.returnHome)
-            returnHome.setOnClickListener {
-
-                val intent: Intent = Intent(this@MyPageChangeCompanyActivity, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-
-            }
         }
     }
 
-    private fun logoTag(i: Int) = "logo$i"
-
-    private fun leftLL(i: Int): LinearLayout {
-        val ll_left = LinearLayout(applicationContext)
-
-        ll_left.orientation = LinearLayout.VERTICAL
-        ll_left.layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-
-        //up
-        val textView = TextView(applicationContext)
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
-        textView.setTextColor(Color.parseColor("#000000"))
-        textView.text = pEtOfficeGetTenant.infoJson().result.tenantlist[i].tenantname
-        ll_left.addView(textView)
-
-
-
-
-        //down
-        val textViewDown = TextView(applicationContext)
-        textViewDown.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14F);
-        textViewDown.setTextColor(Color.parseColor("#000000"))
-        textViewDown.text = pEtOfficeGetTenant.infoJson().result.tenantlist[i].posturl
-        ll_left.addView(textViewDown)
-
-        return ll_left
-    }
-
-    private fun rightLL(): LinearLayout {
-        val ll_right = LinearLayout(applicationContext)
-
-        ll_right.orientation = LinearLayout.VERTICAL
-        ll_right.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-        ll_right.gravity = (Gravity.CENTER or Gravity.RIGHT)
-
-        ll_right.addView(makeImage(100))
-        return ll_right
-    }
-
-
-    private fun makeImage(size:Int):ImageView {
-        val imageView = ImageView(applicationContext)
-        val myDrawable = ResourcesCompat.getDrawable(
-            resources, R.drawable.ic_baseline_account_circle_24_blue, null
-        )
-
-        //image logo size
-        val layoutParams = LinearLayout.LayoutParams(size, size)
-        imageView.layoutParams = layoutParams
-
-        //image logo add
-        imageView.setImageDrawable(myDrawable)
-
-
-        return imageView
-    }
-
     override fun onRefresh() {
+
+        mSwipeRefreshLayout.isRefreshing = false;
+        refreshPage()
     }
 
 
