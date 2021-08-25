de58673c1e8c1d6573cf8f62580bd60096d202d5
chenzhizs <chenzhi@qq.com>
2021-08-25 18:20:04 +0900

    从Api中发送post处理 和 BaseActivity继承 | MyPageChangeCompanyActivity.kt 修改完毕

--- app/src/main/java/com/xieyi/etoffice/common/Api.kt
+++ app/src/main/java/com/xieyi/etoffice/common/Api.kt
@@ -2,9 +2,12 @@ package com.xieyi.etoffice.common
 
 import android.content.Context
 import com.xieyi.etoffice.Config
+import com.xieyi.etoffice.EtOfficeApp
 import com.xieyi.etoffice.common.model.LoginResultModel
 import com.xieyi.etoffice.enum.RequestMethod
 import com.xieyi.etoffice.enum.ResultType
+import com.xieyi.etoffice.jsonData.EtOfficeGetTenant
+import com.xieyi.etoffice.jsonData.EtOfficeSetTenant
 import org.json.JSONObject
 
 class Api {
@@ -48,6 +51,85 @@ class Api {
                 classType = LoginResultModel::class.java as Class<Any>,
                 onSuccess = { data ->
                     val model = data as LoginResultModel
+                    onSuccess(model)
+                },
+                onFailure = { error, data ->
+                    onFailure(error, data)
+                }
+            )
+        }
+
+        /**
+         * EtOfficeGetTenant
+         *
+         * @param context:          コンテキスト
+         * @param onSuccess:        成功コールバック
+         * @param onFailure:        失敗コールバック
+         */
+        @Suppress("UNCHECKED_CAST")
+        fun EtOfficeGetTenantFun(
+            context: Context,
+            onSuccess: onSuccess<EtOfficeGetTenant.JsonClass>,
+            onFailure: onFailure<ResultType, Any>
+        ) {
+            val url: String = Config.ApiUrl
+
+            val jsonObject = JSONObject()
+            jsonObject.put("app", "EtOfficeGetTenant")
+            jsonObject.put("token", EtOfficeApp.Token)
+            jsonObject.put("device", Config.Device)
+
+            HttpUtil.callAsyncHttp(
+                context = context,
+                url = url,
+                method = RequestMethod.POST,
+                parameter = jsonObject,
+                authToken = false,
+                fcmToken = false,
+                classType = EtOfficeGetTenant.JsonClass::class.java as Class<Any>,
+                onSuccess = { data ->
+                    val model = data as EtOfficeGetTenant.JsonClass
+                    onSuccess(model)
+                },
+                onFailure = { error, data ->
+                    onFailure(error, data)
+                }
+            )
+        }
+
+        /**
+         * EtOfficeSetTenant
+         *
+         * @param context:          コンテキスト
+         * @param tenantid:         tenantid
+         * @param onSuccess:        成功コールバック
+         * @param onFailure:        失敗コールバック
+         */
+        @Suppress("UNCHECKED_CAST")
+        fun EtOfficeSetTenantFun(
+            context: Context,
+            tenantid: String,
+            onSuccess: onSuccess<EtOfficeSetTenant.JsonClass>,
+            onFailure: onFailure<ResultType, Any>
+        ) {
+            val url: String = Config.ApiUrl
+
+            val jsonObject = JSONObject()
+            jsonObject.put("app", "EtOfficeSetTenant")
+            jsonObject.put("token", EtOfficeApp.Token)
+            jsonObject.put("device", "android")
+            jsonObject.put("tenantid",tenantid)
+
+            HttpUtil.callAsyncHttp(
+                context = context,
+                url = url,
+                method = RequestMethod.POST,
+                parameter = jsonObject,
+                authToken = false,
+                fcmToken = false,
+                classType = EtOfficeSetTenant.JsonClass::class.java as Class<Any>,
+                onSuccess = { data ->
+                    val model = data as EtOfficeSetTenant.JsonClass
                     onSuccess(model)
                 },
                 onFailure = { error, data ->
--- app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyActivity.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageChangeCompanyActivity.kt
@@ -2,15 +2,16 @@ package com.xieyi.etoffice.ui.MyPage
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.xieyi.etoffice.EtOfficeApp
 import com.xieyi.etoffice.MainActivity
-import com.xieyi.etoffice.R
+import com.xieyi.etoffice.Tools
+import com.xieyi.etoffice.base.BaseActivity
+import com.xieyi.etoffice.common.Api
 import com.xieyi.etoffice.databinding.ActivityMyPageChangeCompanyBinding
 import com.xieyi.etoffice.jsonData.EtOfficeGetTenant
 import com.xieyi.etoffice.jsonData.EtOfficeLogin
@@ -18,7 +19,7 @@ import com.xieyi.etoffice.jsonData.EtOfficeSetTenant
 import kotlinx.coroutines.*
 
 
-class MyPageChangeCompanyActivity : AppCompatActivity(),
+class MyPageChangeCompanyActivity : BaseActivity(),
     SwipeRefreshLayout.OnRefreshListener  {
 
     private val TAG = javaClass.simpleName
@@ -27,9 +28,6 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
     private lateinit var pEtOfficeSetTenant : EtOfficeSetTenant
     private lateinit var pEtOfficeLogin : EtOfficeLogin
 
-
-    //private lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
-    //private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter:GetTenantAdapter
 
     private lateinit var binding: ActivityMyPageChangeCompanyBinding
@@ -44,7 +42,7 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
         pEtOfficeSetTenant = EtOfficeSetTenant()
         pEtOfficeLogin = EtOfficeLogin()
 
-        refreshPage()
+        EtOfficeGetTenantPost()
 
         binding.recyclerViewGetTenant.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
@@ -53,7 +51,7 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
                     if(!recyclerView.canScrollVertically(1)){
                         //Log.e(TAG, "onScrollStateChanged: more date")
                         binding.swipeRefreshLayout.isRefreshing = false
-                        refreshPage()
+                        EtOfficeGetTenantPost()
 
                     }
                 }
@@ -65,90 +63,184 @@ class MyPageChangeCompanyActivity : AppCompatActivity(),
         binding.swipeRefreshLayout.setOnRefreshListener(this);
     }
 
-    private fun refreshPage() {
-        GlobalScope.launch(errorHandler) {
-            withContext(Dispatchers.IO) {
-                //データ更新
-                try {
-                    val r = pEtOfficeGetTenant.post()                                    //Json 送信
-                    Log.e(TAG, "pEtOfficeGetTenant.post() :$r")
 
-                    doOnUiRefresh()
+    private fun EtOfficeGetTenantPost() {
+        Api.EtOfficeGetTenantFun(
+            context = this@MyPageChangeCompanyActivity,
+            onSuccess = { model ->
+                Handler(Looper.getMainLooper()).post {
 
-                } catch (e: Exception) {
-                    Log.e(TAG, "pEtOfficeGetTenant.post()",e)
+                    when (model.status) {
+                        0 -> {
+                            EtOfficeGetTenantResult(model.result)
+                        }
+                        1 -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                        else -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                    }
                 }
-
+            },
+            onFailure = { error, data ->
+                Handler(Looper.getMainLooper()).post {
+                    Log.e(TAG, "onFailure:$data");
+                    //CommonUtil.handleError(it, error, data)
+                }
             }
-        }
+        )
+
     }
 
-    private val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-        // 发生异常时的捕获
+    private fun EtOfficeSetTenantPost(tenantid: String) {
+        Api.EtOfficeSetTenantFun(
+            context = this@MyPageChangeCompanyActivity,
+            tenantid= tenantid,
+            onSuccess = { model ->
+                Handler(Looper.getMainLooper()).post {
+
+                    when (model.status) {
+                        0 -> {
+                            EtOfficeSetTenantResult(model.result)
+                        }
+                        1 -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                        else -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                    }
+                }
+            },
+            onFailure = { error, data ->
+                Handler(Looper.getMainLooper()).post {
+                    Log.e(TAG, "onFailure:$data");
+                    //CommonUtil.handleError(it, error, data)
+                }
+            }
+        )
+
     }
 
     // UI更新
-    private suspend fun doOnUiRefresh() {
-        withContext(Dispatchers.Main) {
-            //Log.e(TAG, "doOnUiRefresh: begin")
+    private  fun EtOfficeGetTenantResult(result: EtOfficeGetTenant.Result) {
+        //Log.e(TAG, "doOnUiRefresh: begin")
 
-            //record_title
-            val recordTitle = binding.recordTitle
-            val tenantid = EtOfficeApp.TenantId
-            val hpid = EtOfficeApp.HpId
-            recordTitle.text = "TENANTID = $tenantid HPID = $hpid"
+        //record_title
+        val recordTitle = binding.recordTitle
+        val tenantid = EtOfficeApp.TenantId
+        val hpid = EtOfficeApp.HpId
+        recordTitle.text = "TENANTID = $tenantid HPID = $hpid"
 
-            //returnHome
-            val returnHome = binding.returnHome
-            returnHome.setOnClickListener {
+        //returnHome
+        val returnHome = binding.returnHome
+        returnHome.setOnClickListener {
 
-                val intent: Intent = Intent(this@MyPageChangeCompanyActivity, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-
-            }
-
-
-            val recyclerView: RecyclerView = binding.recyclerViewGetTenant
-
-            val sortedList = pEtOfficeGetTenant.infoJson().result.tenantlist.sortedWith(compareBy(
-                { it.tenantid }
-                , { it.tenantname }
-                , { it.hpid }
-                , { it.posturl }
-            ))
-            mAdapter=GetTenantAdapter(sortedList)
-
-
-            mAdapter.setOnAdapterListener(object : GetTenantAdapter.OnAdapterListener{
-                override fun onClick(tenantid: String) {
-                    GlobalScope.launch(errorHandler) {
-                        withContext(Dispatchers.IO) {
-                            //データ更新 SetTenant
-                            try {
-                                if(pEtOfficeSetTenant.post(tenantid)=="0"){
-                                    if(pEtOfficeLogin.post(EtOfficeApp.uid,EtOfficeApp.password) == "0"){
-                                        refreshPage()
-                                    }
-                                }
-                            } catch (e: Exception) {
-                                Log.e(TAG, "pEtOfficeSetTenant.post()",e)
-                            }
-
-                        }
-                    }
-
-                }
-            })
-
-            recyclerView.adapter = mAdapter
+            val intent: Intent = Intent(this@MyPageChangeCompanyActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
 
         }
+
+
+        val recyclerView: RecyclerView = binding.recyclerViewGetTenant
+
+        val sortedList = result.tenantlist.sortedWith(compareBy(
+            { it.tenantid }
+            , { it.tenantname }
+            , { it.hpid }
+            , { it.posturl }
+        ))
+        mAdapter=GetTenantAdapter(sortedList)
+
+
+        mAdapter.setOnAdapterListener(object : GetTenantAdapter.OnAdapterListener{
+            override fun onClick(tenantid: String) {
+                EtOfficeSetTenantPost(tenantid)
+            }
+        })
+
+        recyclerView.adapter = mAdapter
+
+    }
+
+    private  fun EtOfficeSetTenantResult(result: EtOfficeSetTenant.Result) {
+        EtOfficeLoginPost(EtOfficeApp.uid,EtOfficeApp.password)
+    }
+
+    private fun EtOfficeLoginPost(uid:String, password:String) {
+        Api.EtOfficeLogin(
+            context = this@MyPageChangeCompanyActivity,
+            uid = uid,
+            password = password,
+            registrationid = "6",
+            onSuccess = { model ->
+                Handler(Looper.getMainLooper()).post {
+
+                    when (model.status) {
+                        0 -> {
+
+                            //data save
+                            EtOfficeApp.TenantId = model.result.tenantid
+                            EtOfficeApp.HpId = model.result.hpid
+                            EtOfficeApp.uid = uid
+                            EtOfficeApp.password = password
+                            EtOfficeApp.userid = model.result.userid
+                            EtOfficeGetTenantPost()
+                        }
+                        1 -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                        else -> {
+                            Tools.msgAlertDialog(
+                                this@MyPageChangeCompanyActivity,
+                                model.status.toString(),
+                                model.message,
+                                "確認"
+                            )
+                        }
+                    }
+                }
+            },
+            onFailure = { error, data ->
+                Handler(Looper.getMainLooper()).post {
+                    Log.e(TAG, "onFailure:$data");
+                    //CommonUtil.handleError(it, error, data)
+                }
+            }
+        )
+
     }
 
     override fun onRefresh() {
         binding.swipeRefreshLayout.isRefreshing = false;
-        refreshPage()
+        EtOfficeGetTenantPost()
     }
 
 
