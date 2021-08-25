2bcada088027a53c574916e1ccb1216f1c85d0ac
chenzhizs <chenzhi@qq.com>
2021-08-25 12:09:08 +0900

    HomeStatusDialog findViewById 转 binding

--- app/src/main/java/com/xieyi/etoffice/ui/home/HomeStatusDialog.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/home/HomeStatusDialog.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.DialogFragment
 import com.xieyi.etoffice.GpsTracker
 import com.xieyi.etoffice.R
 import com.xieyi.etoffice.Tools
+import com.xieyi.etoffice.databinding.DialogHomeStatusBinding
 import com.xieyi.etoffice.jsonData.EtOfficeSetUserLocation
 import com.xieyi.etoffice.jsonData.EtOfficeSetUserStatus
 
@@ -24,8 +25,6 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
 
     private val _statustext = statustext
 
-    private lateinit var mainView: View
-
     private var longitude = 0.0
 
     private var latitude = 0.0
@@ -33,6 +32,7 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
     private lateinit var pEtOfficeSetUserStatus : EtOfficeSetUserStatus
     private lateinit var pEtOfficeSetUserLocation : EtOfficeSetUserLocation
 
+    private lateinit var binding: DialogHomeStatusBinding
 
     private lateinit var gpsTracker: GpsTracker
 
@@ -59,7 +59,7 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
             @Nullable container: ViewGroup?,
             @Nullable savedInstanceState: Bundle?
     ): View {
-        mainView = inflater.inflate(R.layout.dialog_home_status, container)
+        binding = DialogHomeStatusBinding.inflate(inflater, container, false)
 
 
 
@@ -73,13 +73,13 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
 
 
         //ボタン　保存後に閉じる
-        val btnSaveAndClose = mainView.findViewById<TextView>(R.id.btn_cancel_and_close)
+        val btnSaveAndClose = binding.btnCancelAndClose
         btnSaveAndClose.setOnClickListener {
 
-            val etUserLocation: EditText = mainView.findViewById(R.id.user_location)
+            val etUserLocation: EditText = binding.userLocation
             val userLocation: String = etUserLocation.text.toString()
 
-            val etMemo: EditText = mainView.findViewById(R.id.user_status_memo)
+            val etMemo: EditText = binding.userStatusMemo
             val memo: String = etMemo.text.toString()
 
             listener?.onClick(userLocation, memo)
@@ -87,7 +87,7 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
         }
 
         //状態表示
-        val tvState:TextView= mainView.findViewById<TextView>(R.id.tvState)
+        val tvState:TextView= binding.tvState
         tvState.text = _statustext
 
         //set_user_Status
@@ -107,12 +107,12 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
         gpsCheck()
 
 
-        return mainView
+        return binding.root
     }
 
     private fun gpsCheck() {
-        val tvLatitude = mainView.findViewById<TextView>(R.id.tvLatitude)
-        val tvLongitude = mainView.findViewById<TextView>(R.id.tvLongitude)
+        val tvLatitude = binding.tvLatitude
+        val tvLongitude = binding.tvLongitude
         gpsTracker = GpsTracker(activity)
         if (gpsTracker.canGetLocation()) {
             latitude= gpsTracker.getLatitude()
@@ -130,9 +130,9 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
 
     private suspend fun setUserStatus() {
         withContext(Dispatchers.Main) {
-            val etMemo: EditText = mainView.findViewById(R.id.user_status_memo)
-            val setUserStatus: TextView = mainView.findViewById(R.id.set_user_status)
-            val userLocation: EditText = mainView.findViewById(R.id.user_location)
+            val etMemo: EditText = binding.userStatusMemo
+            val setUserStatus: TextView = binding.setUserStatus
+            val userLocation: EditText = binding.userLocation
 
             setUserStatus.setOnClickListener {
                 Log.e(TAG, "setUserStatus.setOnClickListener: begin")
@@ -161,9 +161,9 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
             Log.e(TAG, "pEtOfficeSetUserStatus.post() :$r")
 
             if (r == "0") {
-                Tools.showMsg(mainView, "登録します")
+                Tools.showMsg(binding.root, "登録します")
             } else {
-                Tools.showMsg(mainView, "pEtOfficeSetUserStatus.post() :$r")
+                Tools.showMsg(binding.root, "pEtOfficeSetUserStatus.post() :$r")
             }
         } catch (e: Exception) {
             Log.e(TAG, "pEtOfficeSetUserStatus.post()",e)
@@ -173,8 +173,8 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
 
     private suspend fun setUserLocation() {
         withContext(Dispatchers.Main) {
-            val userLocation: EditText = mainView.findViewById(R.id.user_location)
-            val setUserLocation: TextView = mainView.findViewById<TextView>(R.id.set_user_location)
+            val userLocation: EditText = binding.userLocation
+            val setUserLocation: TextView = binding.setUserLocation
             setUserLocation.setOnClickListener {
                 Log.e(TAG, "setUserLocation.setOnClickListener: begin")
 
@@ -197,9 +197,9 @@ class HomeStatusDialog(statusvalue: String,statustext:String) : DialogFragment()
             Log.e(TAG, "pEtOfficeSetUserLocation.post() :$r")
 
             if (r == "0") {
-                Tools.showMsg(mainView, "登録します")
+                Tools.showMsg(binding.root, "登録します")
             } else {
-                Tools.showMsg(mainView, "pEtOfficeSetUserLocation.post() :$r")
+                Tools.showMsg(binding.root, "pEtOfficeSetUserLocation.post() :$r")
             }
         } catch (e: Exception) {
             Log.e(TAG, "pEtOfficeSetUserLocation.post()",e)
