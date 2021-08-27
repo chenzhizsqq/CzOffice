794c80655f499fe683847919da204cd56a91ee88
chenzhizs <chenzhi@qq.com>
2021-08-27 16:39:36 +0900

    从Api中发送post处理 和 BaseActivity继承 | 清除多余的函数 EtOfficeGetProject.kt EtOfficeSetMessage.kt EtOfficeSetReport.kt EtOfficeSetTenant.kt

--- app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeGetProject.kt
+++ app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeGetProject.kt
@@ -15,69 +15,6 @@ import org.json.JSONObject
 //EtOfficeGetProject プロジェクト一覧
 class EtOfficeGetProject {
 
-    private val TAG = javaClass.simpleName
-    private var lastJson: String = ""
-    val app: String = "EtOfficeGetProject"
-
-    fun post(): String {
-        var status:String = "-1"
-        val client: OkHttpClient = OkHttpClient()
-        val url:String = Config.LoginUrl
-
-        /*
-        {"app":"EtOfficeGetProject"
-        ,"token":"202011291352391050000000090010000000000000010125"
-        ,"device":"ios"
-        ,"tenant":"1"
-        ,"hpid":"8"
-        ,"userid":"2025"
-        ,"ymd":"20210305"}
-         */
-        try {
-            val jsonObject = JSONObject()
-            jsonObject.put("app", app)
-            jsonObject.put("token", EtOfficeApp.Token)
-            jsonObject.put("device", "android")
-            jsonObject.put("tenant", EtOfficeApp.TenantId)
-            jsonObject.put("hpid", EtOfficeApp.HpId)
-            jsonObject.put("userid", EtOfficeApp.userid)
-            jsonObject.put("ymd", "20210305")
-            Log.e(TAG, jsonObject.toString(), )
-
-            val body = jsonObject.toString()
-                .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-
-            val request = Request.Builder().url(url).post(body).build()
-
-            val response: Response = client.newCall(request).execute();
-            if (response.isSuccessful) {
-
-                val json: String = response.body!!.string()
-                lastJson = json
-                val mJsonResult = JSONObject(json)
-                Log.e(TAG, "mJsonResult: :$mJsonResult" )
-
-                status = mJsonResult.getString("status")
-
-
-                return status
-            }else{
-                Log.e(TAG, "postRequest: false" )
-            }
-        }catch (e: Exception){
-            Log.e(TAG, e.toString())
-        }
-        return status
-    }
-
-
-    fun infoJson(): JsonClass {
-        val gson = Gson()
-        val mJson: JsonClass =
-            gson.fromJson(lastJson, JsonClass::class.java)
-        return mJson
-    }
-
     data class JsonClass(
         val message: String,
         val result: Result,
