282e3cf86186602b6723b98804a24d841af01dd1
chenzhizs <chenzhi@qq.com>
2021-08-27 16:26:59 +0900

    从Api中发送post处理 和 BaseActivity继承 | HomeReportDialog.kt 修改完成

--- app/src/main/java/com/xieyi/etoffice/ui/home/HomeReportDialog.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/home/HomeReportDialog.kt
@@ -2,6 +2,8 @@ package com.xieyi.etoffice.ui.home
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.util.TypedValue
 import android.view.*
@@ -13,8 +15,10 @@ import androidx.annotation.NonNull
 import androidx.annotation.Nullable
 import androidx.core.view.setPadding
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.snackbar.Snackbar
 import com.xieyi.etoffice.R
 import com.xieyi.etoffice.Tools
+import com.xieyi.etoffice.common.Api
 import com.xieyi.etoffice.databinding.DialogHomeReportBinding
 import com.xieyi.etoffice.jsonData.EtOfficeGetStatusList
 
@@ -27,7 +31,6 @@ class HomeReportDialog : DialogFragment() {
     private val WRAP_CONTENT = LinearLayout.LayoutParams.WRAP_CONTENT
     private val MATCH_PARENT = LinearLayout.LayoutParams.MATCH_PARENT
 
-    private lateinit var pEtOfficeGetStatusList : EtOfficeGetStatusList
 
     private lateinit var binding: DialogHomeReportBinding
     override fun onCreateView(
@@ -37,22 +40,7 @@ class HomeReportDialog : DialogFragment() {
     ): View {
         binding = DialogHomeReportBinding.inflate(inflater, container, false)
 
-        pEtOfficeGetStatusList = EtOfficeGetStatusList()
-
-
-        GlobalScope.launch(errorHandler) {
-            withContext(Dispatchers.IO) {
-                //データ更新
-                try {
-                    val r = pEtOfficeGetStatusList.post()
-                    Log.e(TAG, "pEtOfficeGetStatusList.post() :$r")
-                } catch (e: Exception) {
-                    Log.e(TAG, "pEtOfficeGetStatusList.post()",e)
-                }
-
-                doOnUiCode()
-            }
-        }
+        EtOfficeGetStatusListPost()
 
         return binding.root
     }
@@ -61,88 +49,114 @@ class HomeReportDialog : DialogFragment() {
         // 发生异常时的捕获
     }
 
-    private suspend fun doOnUiCode() {
-        withContext(Dispatchers.Main) {
-            //フルスクリーン　Full screen
-            val window = dialog!!.window
-            window!!.setBackgroundDrawableResource(android.R.color.transparent)
-            val attributes = window.attributes
-            //attributes.gravity = Gravity.BOTTOM //下方
-            attributes.width = WindowManager.LayoutParams.MATCH_PARENT //满屏
-            window.attributes = attributes
 
+    private fun EtOfficeGetStatusListPost(){
+        Api.EtOfficeGetStatusListPost(
+            context = requireActivity(),
+            onSuccess = { model ->
+                Handler(Looper.getMainLooper()).post {
 
-            //ボタン　保存後に閉じる
-            val btnSaveAndClose = binding.btnSaveAndClose
-            btnSaveAndClose.setOnClickListener {
-                dialog!!.dismiss()
+                    when (model.status) {
+                        0 -> {
+                            EtOfficeGetStatusListResult(model.result)
+                        }
+                        1 -> {
+                            Snackbar.make(
+                                binding.root,
+                                model.message,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        else -> {
+                            Snackbar.make(
+                                binding.root,
+                                model.message,
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+                }
+            },
+            onFailure = { error, data ->
+                Handler(Looper.getMainLooper()).post {
+                    Log.e(TAG, "onFailure:$data");
+                }
             }
-
-            //record_linearLayout
-            val recordLinearLayout = binding.recordLinearLayout
-            recordLinearLayout.setPadding(10)
-
-
-            val size = pEtOfficeGetStatusList.infoJson().result.recordlist.size
-
-            for (i in 0..size - 1) {
-
-                val mLinearLayout = LinearLayout(activity)
-
-                mLinearLayout.setOrientation(LinearLayout.HORIZONTAL)
-
-                val lp = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-                mLinearLayout.layoutParams = lp
-
-
-                //left
-                val textView = TextView(activity)
-                textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
-                textView.setTextColor(Color.parseColor("#000000"))
-                textView.text =
-                    Tools.allDateTime(pEtOfficeGetStatusList.infoJson().result.recordlist[i].statustime)
-
-                mLinearLayout.addView(textView)
-
-
-                //right
-                val textViewRight = TextView(activity)
-                textViewRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
-                textViewRight.layoutParams = FrameLayout.LayoutParams(
-                    TableRow.LayoutParams.MATCH_PARENT,
-                    TableRow.LayoutParams.WRAP_CONTENT
-                )
-                val rightText = (
-                        pEtOfficeGetStatusList.infoJson().result.recordlist[i].statustext
-                        + " " + pEtOfficeGetStatusList.infoJson().result.recordlist[i].memo )
-                textViewRight.text = rightText
-
-                textViewRight.gravity = Gravity.RIGHT;
-                mLinearLayout.addView(textViewRight)
-
-
-
-                mLinearLayout.setPadding(10)
-                recordLinearLayout.addView(mLinearLayout)
-
-
-                //線
-
-                val mLinearLayout2 = LinearLayout(activity)
-                val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
-                mLinearLayout2.layoutParams = lp2
-                mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
-                recordLinearLayout.addView(mLinearLayout2)
-
-            }
-        }
+        )
     }
 
+    private fun EtOfficeGetStatusListResult(result:EtOfficeGetStatusList.Result) {
+        //フルスクリーン　Full screen
+        val window = dialog!!.window
+        window!!.setBackgroundDrawableResource(android.R.color.transparent)
+        val attributes = window.attributes
+        //attributes.gravity = Gravity.BOTTOM //下方
+        attributes.width = WindowManager.LayoutParams.MATCH_PARENT //满屏
+        window.attributes = attributes
 
-    private fun makeTextView(text: String): TextView? {
-        val textView = TextView(activity)
-        textView.text = text
-        textView.setPadding(5, 1, 5, 1)
-        return textView
+
+        //ボタン　保存後に閉じる
+        val btnSaveAndClose = binding.btnSaveAndClose
+        btnSaveAndClose.setOnClickListener {
+            dialog!!.dismiss()
+        }
+
+        //record_linearLayout
+        val recordLinearLayout = binding.recordLinearLayout
+        recordLinearLayout.setPadding(10)
+
+
+        val size = result.recordlist.size
+
+        for (i in 0..size - 1) {
+
+            val mLinearLayout = LinearLayout(activity)
+
+            mLinearLayout.setOrientation(LinearLayout.HORIZONTAL)
+
+            val lp = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+            mLinearLayout.layoutParams = lp
+
+
+            //left
+            val textView = TextView(activity)
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
+            textView.setTextColor(Color.parseColor("#000000"))
+            textView.text =
+                Tools.allDateTime(result.recordlist[i].statustime)
+
+            mLinearLayout.addView(textView)
+
+
+            //right
+            val textViewRight = TextView(activity)
+            textViewRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
+            textViewRight.layoutParams = FrameLayout.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+            )
+            val rightText = (
+                    result.recordlist[i].statustext
+                    + " " + result.recordlist[i].memo )
+            textViewRight.text = rightText
+
+            textViewRight.gravity = Gravity.RIGHT;
+            mLinearLayout.addView(textViewRight)
+
+
+
+            mLinearLayout.setPadding(10)
+            recordLinearLayout.addView(mLinearLayout)
+
+
+            //線
+
+            val mLinearLayout2 = LinearLayout(activity)
+            val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
+            mLinearLayout2.layoutParams = lp2
+            mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
+            recordLinearLayout.addView(mLinearLayout2)
+
+        }
     }
 }
