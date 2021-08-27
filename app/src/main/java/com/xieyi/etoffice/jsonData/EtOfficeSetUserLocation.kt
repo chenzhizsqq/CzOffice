4f451c3d39a8784f489ef0b8e19bc0a393b1a1ec
chenzhizs <chenzhi@qq.com>
2021-08-27 18:24:52 +0900

    全部json处理的格式，已经转过来了

--- app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeSetUserLocation.kt
+++ app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeSetUserLocation.kt
@@ -15,61 +15,6 @@ import org.json.JSONObject
 //EtOfficeSetUserLocation   ユーザー勤務場所設定
 class EtOfficeSetUserLocation {
 
-    val TAG = javaClass.simpleName
-    var lastJson: String = ""
-    val app: String = "EtOfficeSetUserLocation"
-
-    fun post(longitude: String, latitude:String, location:String): String {
-        var status:String = "-1"
-        val client: OkHttpClient = OkHttpClient()
-        val url:String = Config.LoginUrl
-
-        try {
-            val jsonObject = JSONObject()
-            jsonObject.put("app", app)
-            jsonObject.put("token", EtOfficeApp.Token)
-            jsonObject.put("tenant", EtOfficeApp.TenantId)
-            jsonObject.put("hpid", EtOfficeApp.HpId)
-            jsonObject.put("device", "android")
-//                jsonObject.put("longitude", "140.00468200000000")
-//                jsonObject.put("latitude", "35.70148346348169")
-            //jsonObject.put("location", "船橋事務所")
-            jsonObject.put("longitude", longitude)
-            jsonObject.put("latitude", latitude)
-            jsonObject.put("location", location)
-            Log.e(TAG, "post: jsonObject:$jsonObject")
-            val body = jsonObject.toString()
-                .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-
-            val request = Request.Builder().url(url).post(body).build()
-
-            val response: Response = client.newCall(request).execute();
-            if(response.isSuccessful){
-
-                val json:String = response.body!!.string()
-                lastJson = json
-                val mJsonResult = JSONObject(json)
-                Log.e(TAG, "mJsonResult:$mJsonResult" )
-
-                status = mJsonResult.getString("status")
-
-
-                return status
-            }else{
-                Log.e(TAG, "postRequest: false")
-            }
-        } catch (e: Exception) {
-            Log.e(TAG, e.toString())
-        }
-        return status
-    }
-
-    fun infoJson(): JsonClass {
-        val gson = Gson()
-        val mJson: JsonClass =
-            gson.fromJson(lastJson, JsonClass::class.java)
-        return mJson
-    }
     data class JsonClass(
         val message: String,
         val result: Result,
--- app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeSetUserStatus.kt
+++ app/src/main/java/com/xieyi/etoffice/jsonData/EtOfficeSetUserStatus.kt
@@ -15,69 +15,6 @@ import org.json.JSONObject
 //EtOfficeSetUserStatus ユーザー勤務状態の設定
 class EtOfficeSetUserStatus {
 
-    val TAG = javaClass.simpleName
-    var lastJson: String = ""
-    val app: String = "EtOfficeSetUserStatus"
-
-    fun post(
-        longitude:Double,
-        latitude:Double,
-        location:String,
-         statusvalue:String,
-        statustext:String
-         ,memo:String
-    ): String {
-        var status:String = "-1"
-        val client: OkHttpClient = OkHttpClient()
-        val url:String = Config.LoginUrl
-
-        try {
-            val jsonObject = JSONObject()
-            jsonObject.put("app", app)
-            jsonObject.put("token", EtOfficeApp.Token)
-            jsonObject.put("tenant", EtOfficeApp.TenantId)
-            jsonObject.put("hpid", EtOfficeApp.HpId)
-            jsonObject.put("device", "android")
-            jsonObject.put("statusvalue", statusvalue)
-            jsonObject.put("statustext", statustext)
-            jsonObject.put("location", location)
-            jsonObject.put("longitude", longitude.toString())
-            jsonObject.put("latitude", latitude.toString())
-            jsonObject.put("memo", memo)
-            Log.e(TAG, "post: jsonObject:$jsonObject")
-
-            val body = jsonObject.toString()
-                .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-
-            val request = Request.Builder().url(url).post(body).build()
-
-            val response: Response = client.newCall(request).execute();
-            if(response.isSuccessful){
-
-                val json:String = response.body!!.string()
-                lastJson = json
-                val mJsonResult = JSONObject(json)
-                Log.e(TAG, "mJsonResult:$mJsonResult" )
-
-                status = mJsonResult.getString("status")
-
-
-                return status
-            }else{
-                Log.e(TAG, "postRequest: false" )
-            }
-        }catch (e: Exception){
-            Log.e(TAG, e.toString())
-        }
-        return status
-    }
-
-    fun infoJson(): JsonClass {
-        val gson = Gson()
-        val mJson: JsonClass =
-            gson.fromJson(lastJson, JsonClass::class.java)
-        return mJson
-    }
     data class JsonClass(
         val message: String,
         val result: Result,
