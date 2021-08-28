212d5a49b7366e5b4433047fe309682a0978018f
chenzhizs <chenzhi@qq.com>
2021-08-28 10:48:28 +0900

    清空SharedPreferences文件中用户信息 | 消除所有的Activity

--- app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageLogoutDialog.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/MyPage/MyPageLogoutDialog.kt
@@ -1,11 +1,13 @@
 package com.xieyi.etoffice.ui.MyPage
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
 import androidx.fragment.app.DialogFragment
+import com.xieyi.etoffice.Config
 import com.xieyi.etoffice.databinding.DialogMyPageLogoutBinding
 import com.xieyi.etoffice.ui.login.LoginActivity
 
@@ -29,8 +31,14 @@ class MyPageLogoutDialog : DialogFragment() {
         //ボタン　保存後に閉じる
         binding.btnLogout.setOnClickListener {
 
+            //清空SharedPreferences文件中用户信息
+            val prefs = activity?.getSharedPreferences(Config.EtOfficeUser, Context.MODE_PRIVATE)
+            prefs?.edit()?.clear()?.apply();
 
+            //消除所有的Activity
             val intent = Intent(activity, LoginActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+
             startActivity(intent)
             activity?.finish()
         }
