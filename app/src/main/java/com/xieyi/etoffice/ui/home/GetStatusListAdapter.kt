13d729a5feda4f35756d93a255a5327d2cfb701f
chenzhizs <chenzhi@qq.com>
2021-08-31 12:16:35 +0900

    改为：RecyclerView + Adapter。HomeReportDialog.kt | 加上GetStatusListAdapter.kt 和 get_status_list.xml

---
+++ app/src/main/java/com/xieyi/etoffice/ui/home/GetStatusListAdapter.kt
@@ -0,0 +1,45 @@
+package com.xieyi.etoffice.ui.home
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.xieyi.etoffice.Tools
+import com.xieyi.etoffice.common.model.StatusInfo
+import com.xieyi.etoffice.databinding.GetStatusListBinding
+
+
+class GetStatusListAdapter(
+    val recordlist: List<StatusInfo>,
+) : RecyclerView.Adapter<GetStatusListAdapter.ViewHolder>() {
+    val TAG:String = javaClass.simpleName
+
+
+    class ViewHolder(binding: GetStatusListBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val statusTime: TextView = binding.statusTime
+        private val status: TextView = binding.status
+        private val memo: TextView = binding.memo
+
+
+        //bind
+        fun bind(info: StatusInfo) {
+            statusTime.text =Tools.allDateTime(info.statustime)
+            status.text = info.statustext
+            memo.text = info.memo
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = GetStatusListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val viewHolder = ViewHolder(binding)
+
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return recordlist.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(recordlist[position])
+    }
+}
--- app/src/main/java/com/xieyi/etoffice/ui/home/HomeReportDialog.kt
+++ app/src/main/java/com/xieyi/etoffice/ui/home/HomeReportDialog.kt
@@ -1,37 +1,32 @@
 package com.xieyi.etoffice.ui.home
 
-import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.util.TypedValue
 import android.view.*
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TableRow
-import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.annotation.Nullable
-import androidx.core.view.setPadding
 import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.snackbar.Snackbar
-import com.xieyi.etoffice.Tools
 import com.xieyi.etoffice.common.Api
 import com.xieyi.etoffice.common.model.StatusResult
 import com.xieyi.etoffice.databinding.DialogHomeReportBinding
 
-import kotlinx.coroutines.*
 
 
-class HomeReportDialog : DialogFragment() {
+class HomeReportDialog : DialogFragment(),
+    SwipeRefreshLayout.OnRefreshListener {
     private val TAG: String = "HomeReportDialog"
 
-    private val WRAP_CONTENT = LinearLayout.LayoutParams.WRAP_CONTENT
-    private val MATCH_PARENT = LinearLayout.LayoutParams.MATCH_PARENT
+    private lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var mRecyclerView: RecyclerView
 
-
+    private lateinit var mAdapter:GetStatusListAdapter
     private lateinit var binding: DialogHomeReportBinding
+
     override fun onCreateView(
             @NonNull inflater: LayoutInflater,
             @Nullable container: ViewGroup?,
@@ -39,15 +34,41 @@ class HomeReportDialog : DialogFragment() {
     ): View {
         binding = DialogHomeReportBinding.inflate(inflater, container, false)
 
+
+        //フルスクリーン　Full screen
+        val window = dialog!!.window
+        window!!.setBackgroundDrawableResource(android.R.color.transparent)
+        val attributes = window.attributes
+        //attributes.gravity = Gravity.BOTTOM //下方
+        attributes.width = WindowManager.LayoutParams.MATCH_PARENT //满屏
+        window.attributes = attributes
+
+
+        //ボタン　保存後に閉じる
+        val btnSaveAndClose = binding.btnSaveAndClose
+        btnSaveAndClose.setOnClickListener {
+            dialog!!.dismiss()
+        }
+
+        mSwipeRefreshLayout= binding.swipeRefreshLayout
+
+        // Listenerをセット
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mRecyclerView = binding.recyclerView
+
+
         EtOfficeGetStatusListPost()
 
         return binding.root
     }
 
-    private val errorHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
-        // 发生异常时的捕获
-    }
 
+    private fun EtOfficeGetStatusListResult(result: StatusResult) {
+        mAdapter= GetStatusListAdapter(result.recordlist)
+        binding.recyclerView.adapter = mAdapter
+
+    }
 
     private fun EtOfficeGetStatusListPost(){
         Api.EtOfficeGetStatusList(
@@ -84,78 +105,8 @@ class HomeReportDialog : DialogFragment() {
         )
     }
 
-    private fun EtOfficeGetStatusListResult(result: StatusResult) {
-        //フルスクリーン　Full screen
-        val window = dialog!!.window
-        window!!.setBackgroundDrawableResource(android.R.color.transparent)
-        val attributes = window.attributes
-        //attributes.gravity = Gravity.BOTTOM //下方
-        attributes.width = WindowManager.LayoutParams.MATCH_PARENT //满屏
-        window.attributes = attributes
-
-
-        //ボタン　保存後に閉じる
-        val btnSaveAndClose = binding.btnSaveAndClose
-        btnSaveAndClose.setOnClickListener {
-            dialog!!.dismiss()
-        }
-
-        //record_linearLayout
-        val recordLinearLayout = binding.recordLinearLayout
-        recordLinearLayout.setPadding(10)
-
-
-        val size = result.recordlist.size
-
-        for (i in 0..size - 1) {
-
-            val mLinearLayout = LinearLayout(activity)
-
-            mLinearLayout.setOrientation(LinearLayout.HORIZONTAL)
-
-            val lp = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
-            mLinearLayout.layoutParams = lp
-
-
-            //left
-            val textView = TextView(activity)
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
-            textView.setTextColor(Color.parseColor("#000000"))
-            textView.text =
-                Tools.allDateTime(result.recordlist[i].statustime)
-
-            mLinearLayout.addView(textView)
-
-
-            //right
-            val textViewRight = TextView(activity)
-            textViewRight.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20F);
-            textViewRight.layoutParams = FrameLayout.LayoutParams(
-                TableRow.LayoutParams.MATCH_PARENT,
-                TableRow.LayoutParams.WRAP_CONTENT
-            )
-            val rightText = (
-                    result.recordlist[i].statustext
-                    + " " + result.recordlist[i].memo )
-            textViewRight.text = rightText
-
-            textViewRight.gravity = Gravity.RIGHT;
-            mLinearLayout.addView(textViewRight)
-
-
-
-            mLinearLayout.setPadding(10)
-            recordLinearLayout.addView(mLinearLayout)
-
-
-            //線
-
-            val mLinearLayout2 = LinearLayout(activity)
-            val lp2 = LinearLayout.LayoutParams(MATCH_PARENT, 1)
-            mLinearLayout2.layoutParams = lp2
-            mLinearLayout2.setBackgroundColor(Color.parseColor("#656565"))
-            recordLinearLayout.addView(mLinearLayout2)
-
-        }
+    override fun onRefresh() {
+        binding.swipeRefreshLayout.isRefreshing = false;
+        EtOfficeGetStatusListPost()
     }
 }
