30e02c92f2a7fa824e5837fe09ab397070847f19
chenzhi <chenzhi@qq.com>
2022-03-24 10:16:13 +0900

    49	全体 CustomCheckBox代码请调整为Ktloin语言

--- app/src/main/java/com/xieyi/etoffice/widget/CustomCheckBox.kt
+++ app/src/main/java/com/xieyi/etoffice/widget/CustomCheckBox.kt
@@ -1,334 +1,316 @@
-package com.xieyi.etoffice.widget;
+package com.xieyi.etoffice.widget
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.util.AttributeSet;
-import android.view.View;
+import android.content.Context
+import kotlin.jvm.JvmOverloads
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.xieyi.etoffice.R
 
-import com.xieyi.etoffice.R;
-
-import androidx.annotation.Nullable;
-
-public class CustomCheckBox extends View {
-    /**
-     * View默认最小宽度
-     */
-    private static final int DEFAULT_MIN_WIDTH = 80;
-
-    /**
-     * 普通风格
-     */
-    private static final int STYLE_NORMAL = 1;
-    /**
-     * 镂空风格
-     */
-    private static final int STYLE_HOLLOW_OUT = 2;
-
+class CustomCheckBox @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
     /**
      * 控件宽
      */
-    private int mViewWidth;
+    private var mViewWidth = 0
+
     /**
      * 控件高
      */
-    private int mViewHeight;
+    private var mViewHeight = 0
+
     /**
      * 原型半径
      */
-    private float mRadius;
+    private var mRadius = 0f
+
     /**
      * 画笔
      */
-    private Paint mPaint;
+    private var mPaint: Paint? = null
+
     /**
      * 钩子的线长度
      */
-    private float mHookLineLength;
+    private var mHookLineLength = 0f
+
     /**
      * 是否选中
      */
-    private boolean isCheck;
+    var isCheck = false
+
     /**
      * 选中时，圆的颜色
      */
-    private int mCheckCircleColor;
+    private var mCheckCircleColor = 0
+
     /**
      * 未选中时，圆的颜色
      */
-    private int mUncheckCircleColor;
+    private var mUncheckCircleColor = 0
+
     /**
      * 选中时，钩子的颜色
      */
-    private int mCheckHookColor;
+    private var mCheckHookColor = 0
+
     /**
      * 未选中时，钩子的颜色
      */
-    private int mUncheckHookColor;
+    private var mUncheckHookColor = 0
+
     /**
      * 混合模式
      */
-    private PorterDuffXfermode mPorterDuffXfermode;
+    private var mPorterDuffXfermode: PorterDuffXfermode? = null
+
     /**
      * 线宽
      */
-    private float mLineWidth;
+    private var mLineWidth = 0f
+
     /**
      * 风格策略
      */
-    private BaseStyleStrategy mStyleStrategy;
+    private var mStyleStrategy: BaseStyleStrategy? = null
+
     /**
      * 切换改变回调
      */
-    private OnCheckChangeListener mCheckListener;
-
-    public CustomCheckBox(Context context) {
-        this(context, null);
-    }
-
-    public CustomCheckBox(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public CustomCheckBox(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr);
-    }
-
-    public static int dip2px(Context context, float dipValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dipValue * scale + 0.5f);
-    }
-
-    private void init(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        initAttr(context, attrs, defStyleAttr);
+    private var mCheckListener: OnCheckChangeListener? = null
+    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+        initAttr(context, attrs, defStyleAttr)
         //画笔
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setColor(mUncheckCircleColor);
-        mPaint.setStrokeWidth(mLineWidth);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint = Paint()
+        mPaint!!.isAntiAlias = true
+        mPaint!!.style = Paint.Style.FILL
+        mPaint!!.color = mUncheckCircleColor
+        mPaint!!.strokeWidth = mLineWidth
+        mPaint!!.strokeJoin = Paint.Join.ROUND
+        mPaint!!.strokeCap = Paint.Cap.ROUND
         //View禁用掉GPU硬件加速，切换到软件渲染模式
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        mPorterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.XOR);
+        setLayerType(LAYER_TYPE_SOFTWARE, null)
+        mPorterDuffXfermode = PorterDuffXfermode(PorterDuff.Mode.XOR)
         //设置点击事件
-        setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        setOnClickListener { }
     }
 
-    private void initAttr(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    private fun initAttr(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
         //默认的选中颜色
-        int defaultCheckCircleColor = Color.argb(255, 254, 201, 77);
+        val defaultCheckCircleColor = Color.argb(255, 254, 201, 77)
         //默认的未选中颜色
-        int defaultUncheckCircleColor = Color.argb(255, 234, 234, 234);
+        val defaultUncheckCircleColor = Color.argb(255, 234, 234, 234)
         //默认选中的钩子颜色
-        int defaultCheckHookColor = Color.argb(255, 53, 40, 33);
+        val defaultCheckHookColor = Color.argb(255, 53, 40, 33)
         //默认未选中的钩子颜色
-        int defaultUncheckHookColor = Color.argb(255, 255, 255, 255);
+        val defaultUncheckHookColor = Color.argb(255, 255, 255, 255)
         //默认风格
-        int defaultStyle = STYLE_NORMAL;
+        val defaultStyle = STYLE_NORMAL
         //线宽
-        float defaultLineWidth = dip2px(context, 1.5f);
+        val defaultLineWidth = dip2px(context, 1.5f).toFloat()
         //风格
-        int style;
+        val style: Int
         if (attrs != null) {
-            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CustomCheckBox, defStyleAttr, 0);
-            mCheckCircleColor = array.getColor(R.styleable.CustomCheckBox_hcb_check_circle_color, defaultCheckCircleColor);
-            mUncheckCircleColor = array.getColor(R.styleable.CustomCheckBox_hcb_uncheck_circle_color, defaultUncheckCircleColor);
-            mCheckHookColor = array.getColor(R.styleable.CustomCheckBox_hcb_check_hook_color, defaultCheckHookColor);
-            mUncheckHookColor = array.getColor(R.styleable.CustomCheckBox_hcb_uncheck_hook_color, defaultUncheckHookColor);
-            style = array.getInt(R.styleable.CustomCheckBox_hcb_style, defaultStyle);
-            isCheck = array.getBoolean(R.styleable.CustomCheckBox_hcb_is_check, false);
-            mLineWidth = array.getDimension(R.styleable.CustomCheckBox_hcb_line_width, defaultLineWidth);
-            array.recycle();
+            val array =
+                context.obtainStyledAttributes(attrs, R.styleable.CustomCheckBox, defStyleAttr, 0)
+            mCheckCircleColor = array.getColor(
+                R.styleable.CustomCheckBox_hcb_check_circle_color,
+                defaultCheckCircleColor
+            )
+            mUncheckCircleColor = array.getColor(
+                R.styleable.CustomCheckBox_hcb_uncheck_circle_color,
+                defaultUncheckCircleColor
+            )
+            mCheckHookColor = array.getColor(
+                R.styleable.CustomCheckBox_hcb_check_hook_color,
+                defaultCheckHookColor
+            )
+            mUncheckHookColor = array.getColor(
+                R.styleable.CustomCheckBox_hcb_uncheck_hook_color,
+                defaultUncheckHookColor
+            )
+            style = array.getInt(R.styleable.CustomCheckBox_hcb_style, defaultStyle)
+            isCheck = array.getBoolean(R.styleable.CustomCheckBox_hcb_is_check, false)
+            mLineWidth =
+                array.getDimension(R.styleable.CustomCheckBox_hcb_line_width, defaultLineWidth)
+            array.recycle()
         } else {
-            mCheckCircleColor = defaultCheckCircleColor;
-            mUncheckCircleColor = defaultUncheckCircleColor;
-            mCheckHookColor = defaultCheckHookColor;
-            mUncheckHookColor = defaultUncheckHookColor;
-            style = defaultStyle;
-            mLineWidth = defaultLineWidth;
-            isCheck = false;
+            mCheckCircleColor = defaultCheckCircleColor
+            mUncheckCircleColor = defaultUncheckCircleColor
+            mCheckHookColor = defaultCheckHookColor
+            mUncheckHookColor = defaultUncheckHookColor
+            style = defaultStyle
+            mLineWidth = defaultLineWidth
+            isCheck = false
         }
         if (style == STYLE_HOLLOW_OUT) {
-            mStyleStrategy = new HollowOutStyleStrategy();
+            mStyleStrategy = HollowOutStyleStrategy()
         } else if (style == STYLE_NORMAL) {
-            mStyleStrategy = new NormalStyleStrategy();
+            mStyleStrategy = NormalStyleStrategy()
         }
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mViewWidth = w;
-        mViewHeight = h;
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mViewWidth = w
+        mViewHeight = h
         //计算圆的半径
-        mRadius = (Math.min(mViewWidth, mViewHeight) / 2f) * 0.90f;
+        mRadius = Math.min(mViewWidth, mViewHeight) / 2f * 0.90f
         //计算对勾的长度
-        mHookLineLength = (Math.min(mViewWidth, mViewHeight) / 2f) * 0.8f;
+        mHookLineLength = Math.min(mViewWidth, mViewHeight) / 2f * 0.8f
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        float left = -mViewWidth / 2f;
-        float top = -mViewHeight / 2f;
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val left = -mViewWidth / 2f
+        val top = -mViewHeight / 2f
         //保存图层
-        int layerId = canvas.saveLayer(left, top, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        val layerId = canvas.saveLayer(
+            left,
+            top,
+            width.toFloat(),
+            height.toFloat(),
+            null,
+            Canvas.ALL_SAVE_FLAG
+        )
         //将画布中心移动到中心点
-        canvas.translate(mViewWidth / 2, mViewHeight / 2);
+        canvas.translate((mViewWidth / 2).toFloat(), (mViewHeight / 2).toFloat())
         //画圆形背景
-        mStyleStrategy.drawCircleBg(canvas);
+        mStyleStrategy!!.drawCircleBg(canvas)
         //画钩子
-        mStyleStrategy.drawHook(canvas);
+        mStyleStrategy!!.drawHook(canvas)
         //恢复图层
-        canvas.restoreToCount(layerId);
+        canvas.restoreToCount(layerId)
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(handleMeasure(widthMeasureSpec), handleMeasure(heightMeasureSpec));
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(handleMeasure(widthMeasureSpec), handleMeasure(heightMeasureSpec))
     }
 
     /**
      * 处理MeasureSpec
      */
-    private int handleMeasure(int measureSpec) {
-        int result = DEFAULT_MIN_WIDTH;
-        int specMode = MeasureSpec.getMode(measureSpec);
-        int specSize = MeasureSpec.getSize(measureSpec);
+    private fun handleMeasure(measureSpec: Int): Int {
+        var result = DEFAULT_MIN_WIDTH
+        val specMode = MeasureSpec.getMode(measureSpec)
+        val specSize = MeasureSpec.getSize(measureSpec)
         if (specMode == MeasureSpec.EXACTLY) {
-            result = specSize;
+            result = specSize
         } else {
             //处理wrap_content的情况
             if (specMode == MeasureSpec.AT_MOST) {
-                result = Math.min(result, specSize);
+                result = Math.min(result, specSize)
             }
         }
-        return result;
+        return result
     }
 
-    @Override
-    public void setOnClickListener(@Nullable OnClickListener l) {
-        super.setOnClickListener(new OnClickWrapper(l));
+    override fun setOnClickListener(l: OnClickListener?) {
+        super.setOnClickListener(OnClickWrapper(l))
     }
 
-    public boolean isCheck() {
-        return isCheck;
-    }
-
-    public CustomCheckBox setCheck(boolean check) {
-        isCheck = check;
+    public fun setCheck(check: Boolean): CustomCheckBox {
+        isCheck = check
         if (mCheckListener != null) {
-            mCheckListener.onCheckChange(check);
+            mCheckListener!!.onCheckChange(check)
         }
-        return this;
+        return this
     }
 
-    public void setOnCheckChangeListener(OnCheckChangeListener listener) {
-        mCheckListener = listener;
+    fun setOnCheckChangeListener(listener: OnCheckChangeListener?) {
+        mCheckListener = listener
     }
 
-    public interface OnCheckChangeListener {
+    interface OnCheckChangeListener {
         /**
          * 切换时回调
          *
          * @param isCheck 是否选中
          */
-        void onCheckChange(boolean isCheck);
+        fun onCheckChange(isCheck: Boolean)
     }
 
-    private class BaseStyleStrategy {
+    private open inner class BaseStyleStrategy {
         /**
          * 画圆形背景
          */
-        protected void drawCircleBg(Canvas canvas) {
+        open fun drawCircleBg(canvas: Canvas) {
             //设置背景圆的颜色
             if (isCheck) {
-                mPaint.setColor(mCheckCircleColor);
+                mPaint!!.color = mCheckCircleColor
             } else {
-                mPaint.setColor(mUncheckCircleColor);
+                mPaint!!.color = mUncheckCircleColor
             }
-            canvas.drawCircle(0, 0, mRadius, mPaint);
+            canvas.drawCircle(0f, 0f, mRadius, mPaint!!)
         }
 
         /**
          * 画钩子
          */
-        protected void drawHook(Canvas canvas) {
+        open fun drawHook(canvas: Canvas) {
             //设置钩子的颜色
             if (isCheck) {
-                mPaint.setColor(mCheckHookColor);
+                mPaint!!.color = mCheckHookColor
             } else {
-                mPaint.setColor(mUncheckHookColor);
+                mPaint!!.color = mUncheckHookColor
             }
             //画钩子要用描边风格
-            mPaint.setStyle(Paint.Style.STROKE);
-            canvas.save();
+            mPaint!!.style = Paint.Style.STROKE
+            canvas.save()
             //画布向下平移一半的半径长度
-            canvas.translate(-(mRadius / 8f), mRadius / 3f);
+            canvas.translate(-(mRadius / 8f), mRadius / 3f)
             //旋转画布45度
-            canvas.rotate(-45);
-            Path path = new Path();
-            path.reset();
-            path.moveTo(0, 0);
+            canvas.rotate(-45f)
+            val path = Path()
+            path.reset()
+            path.moveTo(0f, 0f)
             //向右画一条线
-            path.lineTo(mHookLineLength, 0);
+            path.lineTo(mHookLineLength, 0f)
             //回到中心点
-            path.moveTo(0, 0);
+            path.moveTo(0f, 0f)
             //向上画一条线
-            path.lineTo(0, -mHookLineLength / 2f);
+            path.lineTo(0f, -mHookLineLength / 2f)
             //画路径
-            canvas.drawPath(path, mPaint);
-            canvas.restore();
+            canvas.drawPath(path, mPaint!!)
+            canvas.restore()
         }
     }
 
-    private class NormalStyleStrategy extends BaseStyleStrategy {
-        @Override
-        protected void drawCircleBg(Canvas canvas) {
+    private inner class NormalStyleStrategy : BaseStyleStrategy() {
+        override fun drawCircleBg(canvas: Canvas) {
             //普通风格用填充风格
-            mPaint.setStyle(Paint.Style.FILL);
-            super.drawCircleBg(canvas);
+            mPaint!!.style = Paint.Style.FILL
+            super.drawCircleBg(canvas)
         }
     }
 
     /**
      * 镂空风格
      */
-    private class HollowOutStyleStrategy extends BaseStyleStrategy {
-        @Override
-        protected void drawCircleBg(Canvas canvas) {
+    private inner class HollowOutStyleStrategy : BaseStyleStrategy() {
+        override fun drawCircleBg(canvas: Canvas) {
             if (isCheck) {
                 //镂空风格，选中时用填充
-                mPaint.setStyle(Paint.Style.FILL);
+                mPaint!!.style = Paint.Style.FILL
             } else {
                 //镂空风格，未选中时用描边
-                mPaint.setStyle(Paint.Style.STROKE);
+                mPaint!!.style = Paint.Style.STROKE
             }
-            super.drawCircleBg(canvas);
+            super.drawCircleBg(canvas)
         }
 
-        @Override
-        protected void drawHook(Canvas canvas) {
+        override fun drawHook(canvas: Canvas) {
             //镂空风格，选中时，才画钩子
             if (isCheck) {
                 //设置混合模式
-                mPaint.setXfermode(mPorterDuffXfermode);
-                super.drawHook(canvas);
+                mPaint!!.xfermode = mPorterDuffXfermode
+                super.drawHook(canvas)
                 //去除混合模式
-                mPaint.setXfermode(null);
+                mPaint!!.xfermode = null
             }
         }
     }
@@ -336,26 +318,48 @@ public class CustomCheckBox extends View {
     /**
      * 点击事件包裹，避免外部设置点击事件将内部的切换事件替换
      */
-    private class OnClickWrapper implements OnClickListener {
-        private OnClickListener mOriginListener;
+    private inner class OnClickWrapper : OnClickListener {
+        private var mOriginListener: OnClickListener? = null
 
-        OnClickWrapper() {
+        internal constructor() {}
+        internal constructor(originListener: OnClickListener?) {
+            mOriginListener = originListener
         }
 
-        OnClickWrapper(OnClickListener originListener) {
-            mOriginListener = originListener;
-        }
-
-        @Override
-        public void onClick(View view) {
-            isCheck = !isCheck;
-            invalidate();
+        override fun onClick(view: View) {
+            isCheck = !isCheck
+            invalidate()
             if (mCheckListener != null) {
-                mCheckListener.onCheckChange(isCheck);
+                mCheckListener!!.onCheckChange(isCheck)
             }
             if (mOriginListener != null) {
-                mOriginListener.onClick(view);
+                mOriginListener!!.onClick(view)
             }
         }
+    }
+
+    companion object {
+        /**
+         * View默认最小宽度
+         */
+        private const val DEFAULT_MIN_WIDTH = 80
+
+        /**
+         * 普通风格
+         */
+        private const val STYLE_NORMAL = 1
+
+        /**
+         * 镂空风格
+         */
+        private const val STYLE_HOLLOW_OUT = 2
+        fun dip2px(context: Context, dipValue: Float): Int {
+            val scale = context.resources.displayMetrics.density
+            return (dipValue * scale + 0.5f).toInt()
+        }
+    }
+
+    init {
+        init(context, attrs, defStyleAttr)
     }
 }
